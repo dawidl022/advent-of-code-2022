f33c4c4d4f2c2d95002fe96454bd76c1e72f914d
Dawid Lachowicz <domdaw7@gmail.com>
2022-12-01 06:21:44 +0100

    Complete day 1 in terribly hacky Rust style

--- day-01/src/bin/part1.rs
+++ day-01/src/bin/part1.rs
@@ -1,6 +1,30 @@
-use day_01::domain::placeholder;
+use std::io::{self, BufRead};
 
 fn main() {
-    let message = placeholder();
-    println!("{message}");
+    let stdin = io::stdin();
+    let mut lines = stdin.lock().lines();
+
+    let mut max_cal = 0;
+    let mut cal = 0;
+
+    while let Some(line) = lines.next() {
+        let length: i32 = line.unwrap().trim().parse().unwrap();
+
+        for _ in 0..length {
+            let line = lines
+                .next()
+                .expect("there was no next line")
+                .expect("the line could not be read");
+
+            if line == "" {
+                if cal > max_cal {
+                    max_cal = cal
+                }
+                cal = 0
+            } else {
+                cal += line.parse::<i32>().unwrap()
+            }
+            dbg!("{}", max_cal);
+        }
+    }
 }
--- day-01/src/bin/part2.rs
+++ day-01/src/bin/part2.rs
@@ -1,6 +1,39 @@
-use day_01::domain::placeholder;
+use std::io::{self, BufRead};
 
 fn main() {
-    let message = placeholder();
-    println!("{message}");
+    let stdin = io::stdin();
+    let mut lines = stdin.lock().lines();
+
+    let mut max_cal1 = 0;
+    let mut max_cal2 = 0;
+    let mut max_cal3 = 0;
+    let mut cal = 0;
+
+    while let Some(line) = lines.next() {
+        let length: i32 = line.unwrap().trim().parse().unwrap();
+
+        for _ in 0..length {
+            let line = lines
+                .next()
+                .expect("there was no next line")
+                .expect("the line could not be read");
+
+            if line == "" {
+                if cal > max_cal1 {
+                    max_cal3 = max_cal2;
+                    max_cal2 = max_cal1;
+                    max_cal1 = cal;
+                } else if cal > max_cal2 {
+                    max_cal3 = max_cal2;
+                    max_cal2 = cal;
+                } else if cal > max_cal3 {
+                    max_cal3 = cal;
+                }
+                cal = 0
+            } else {
+                cal += line.parse::<i32>().unwrap()
+            }
+            dbg!("{}", max_cal1 + max_cal2 + max_cal3);
+        }
+    }
 }
